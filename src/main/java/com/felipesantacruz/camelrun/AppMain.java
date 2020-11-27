f63643007af7bf74a131c9b6f23930e999b56216
Felipe <fesanmar@gmail.com>
2020-11-27 11:01:04 +0100

    Refactor main and CLIIntegerFetcher to use a enum by name CLIProperty

--- src/main/java/com/felipesantacruz/camelrun/AppMain.java
+++ src/main/java/com/felipesantacruz/camelrun/AppMain.java
@@ -1,28 +1,24 @@
 package com.felipesantacruz.camelrun;
 
+import static com.felipesantacruz.camelrun.cli.CLIProperty.GOAL;
+import static com.felipesantacruz.camelrun.cli.CLIProperty.NUMBER_OF_PLAYERS;
+
 import java.util.Scanner;
-import java.util.function.Predicate;
 
 import com.felipesantacruz.camelrun.cli.CLIIntegerFetcher;
+import com.felipesantacruz.camelrun.cli.CLIProperty;
 import com.felipesantacruz.camelrun.game.ClassicGame;
 import com.felipesantacruz.camelrun.game.Game;
 
 public class AppMain
 {
-
-	private static final String NUMBER_OF_PLAYERS_QUESTION = "Introduce el número de jugadores "
-			+ "(al menos deben competir dos jugadores):";
-	private static final String WRONG_PLAYERS_NUMBER_MESSAGE = "¡Error! Debes elegir al menos dos jugadores.";
-	private static final Predicate<Integer> PLAYERS_PREDICATE = players -> players >= 2;
-	private static final String GOAL_QUESTION = "Introduce la longitud del reccorido en posiciones (no puede ser menor a uno):";
-	private static final String WRONG_GOAL_MESSAGE = "¡Error! La longitud del recorrido no puede ser menor a una posición.";
-	private static final Predicate<Integer> GOAL_PREDICATE = goal -> goal > 0;
 	
 	static final String WELCOME_MESSAGE = "Bienvenido a CamelRun, la aplicación de"
 			+ " simulación de carreras de camellos.\n"
 			+ "Para abandonar la aplicación, introduce cualaquier carácter no numérico. Sólo se permiten números enteros.\n";
-	private static CLIIntegerFetcher playerFetcher;
-	private static CLIIntegerFetcher goalFetcher;
+	private static int numberOfPlayers;
+	private static int goal;
+	private static Scanner scanner = new Scanner(System.in);
 	
 	public static void main(String[] args) throws InterruptedException
 	{
@@ -33,42 +29,21 @@ public class AppMain
 	private static void askUserToEnterPlayersAndGoal()
 	{
 		System.out.println(WELCOME_MESSAGE);
-		Scanner scanner = new Scanner(System.in);
-		buildFetchersWith(scanner);
-		playerFetcher.askForInteger();
-		goalFetcher.askForInteger();
+		numberOfPlayers = fetchProperty(NUMBER_OF_PLAYERS);
+		goal = fetchProperty(GOAL);
 		scanner.close();
 	}
+	
+	private static int fetchProperty(CLIProperty property)
+	{
+		CLIIntegerFetcher fetcher = new CLIIntegerFetcher(property, scanner);
+		fetcher.askForInteger();
+		return fetcher.getValue();
+	}
 
-	private static void buildFetchersWith(Scanner scanner)
-	{
-		buildPlayersFetcher(scanner);
-		buildGoalFetcher(scanner);
-	}
-	
-	private static void buildPlayersFetcher(Scanner scanner)
-	{
-		playerFetcher = new CLIIntegerFetcher.Builder()
-				.askingFor(NUMBER_OF_PLAYERS_QUESTION)
-				.mustMeetCondition(PLAYERS_PREDICATE)
-				.onErrorWillDisplayMessage(WRONG_PLAYERS_NUMBER_MESSAGE)
-				.useReader(scanner)
-				.build();
-	}
-	
-	private static void buildGoalFetcher(Scanner scanner)
-	{
-		goalFetcher = new CLIIntegerFetcher.Builder()
-				.askingFor(GOAL_QUESTION)
-				.mustMeetCondition(GOAL_PREDICATE)
-				.onErrorWillDisplayMessage(WRONG_GOAL_MESSAGE)
-				.useReader(scanner)
-				.build();
-	}
-	
 	private static void initializeGame()
 	{
-		Game game = new ClassicGame(playerFetcher.getValue(), goalFetcher.getValue());
+		Game game = new ClassicGame(numberOfPlayers, goal);
 		System.out.println("¡Comienza la carrera!");
 		game.startGame();
 	}
---
+++ src/main/java/com/felipesantacruz/camelrun/cli/CLIProperty.java
@@ -0,0 +1,53 @@
+package com.felipesantacruz.camelrun.cli;
+
+import java.util.function.Predicate;
+
+public enum CLIProperty
+{
+	NUMBER_OF_PLAYERS
+	{
+		@Override
+		public String question()
+		{
+			return "Introduce el número de jugadores " + "(al menos deben competir dos jugadores):";
+		}
+
+		@Override
+		public String erroMessage()
+		{
+			return "¡Error! Debes elegir al menos dos jugadores.";
+		}
+
+		@Override
+		public Predicate<Integer> conditionToMeet()
+		{
+			return players -> players >= 2;
+		}
+	},
+	GOAL
+	{
+		@Override
+		public String question()
+		{
+			return "Introduce la longitud del reccorido en posiciones (no puede ser menor a uno):";
+		}
+
+		@Override
+		public String erroMessage()
+		{
+			return "¡Error! La longitud del recorrido no puede ser menor a una posición.";
+		}
+
+		@Override
+		public Predicate<Integer> conditionToMeet()
+		{
+			return goal -> goal > 0;
+		}
+	};
+
+	public abstract String question();
+
+	public abstract String erroMessage();
+
+	public abstract Predicate<Integer> conditionToMeet();
+}
